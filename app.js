6830ab85fd5bb622ebc0d2ef5b0d78b012903050
Rofayda <rofaydabassem@gmail.com>
2024-06-27 01:30:53 +0300

    feat: integrate swagger for api documentation

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exchangeRoutes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
 
 // Load environment variables from .env file as it contains the token for the external API use
 require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 // Middleware Setup
 app.use(bodyParser.json());
 app.use('/api', exchangeRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
